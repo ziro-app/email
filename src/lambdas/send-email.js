d8595eec270a8e8101541502ec7f8b513c6a849d
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-06-17 15:58:30 -0300

    Structural changes

--- src/lambdas/send-email.js
+++ src/lambdas/send-email.js
@@ -1,7 +1,12 @@
 const lambda = require('../templates/lambda')
 const request = require('../templates/request')
+const multipleRequest = require('../templates/multipleRequest')
 
-const sendEmail = ({ body }) => request(body);
+const sendEmail = ({ body }) => {
+    const { customEmail } = body;
+    if (customEmail) return multipleRequest(body);
+    else return request(body);
+};
 
 // export
 const handler = lambda(sendEmail)
