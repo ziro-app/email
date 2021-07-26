10acf5ee0a287c3946c39679bafae25f51cd42bb
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-07-26 15:00:44 -0300

    fix: Removal of link functionality

--- src/models/changeEmail.js
+++ src/models/changeEmail.js
@@ -1,15 +1,7 @@
-const changeEmail = (name, newEmail, link) => `
+const changeEmail = (name, newEmail) => `
     Olá, <strong>${name ? name : ""}</strong><br />
 
     <p>Seu e-mail para fazer login no One App foi alterado para ${newEmail}.</p>
-
-    ${
-      link
-        ? `<p>Se você não solicitou a alteração do seu e-mail de login, clique neste link para redefini-lo.<p/>
-    
-    <a href="${link}">${link}<a><br /><br />`
-        : ""
-    }
 
     <strong>Obrigado,<br />
 
