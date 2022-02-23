73069628adda713e5b3abe7f721f46d5c23552a0
Wermeson Rocha <wermesonrocha2@gmail.com>
2022-02-23 09:29:34 -0300

    feat: Added status update email

---
+++ src/models/statusUpdate.js
@@ -0,0 +1,44 @@
+const { fontTitle } = require("@ziro/theme");
+
+const body = (retailer, supplier, value, status) => {
+  const body = `
+    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+        <tr>
+            <td style="color: #222; font-family: ${fontTitle};">
+                <h1 style="font-size: 24px; margin: 0;">${
+                  retailer ? `Olá, ${retailer}!` : "Olá!"
+                }</h1>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div style="color: #222; font-family: ${fontTitle}; font-size: 14px; line-height: 24px; padding: 5px;" >
+                Seu pagamento ${
+                  supplier ? `na <strong>${supplier}</strong>` : ""
+                } ${
+    value ? `no valor de <strong>${value}</strong> ` : ""
+  }foi finalizado com status: ${
+    status === "Aprovado"
+      ? `<strong style="color: #4BCA81;">${status}</strong>`
+      : `<strong style="color: #EB5757;">${status}</strong>`
+  }. Para mais informações basta consultar os detalhes no app
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div style="color: #222; font-family: ${fontTitle}; font-size: 14px; line-height: 24px; padding: 5px;" >
+                Abraços, <br />
+                Equipe One App
+                </div>
+            </td>
+        </tr>
+    </table>`;
+  return {
+    body,
+    title: "Seu pagamento acaba de atualizar o status",
+    caption: "Notificação de atualização",
+  };
+};
+
+module.exports = body;
--- src/templates/request.js
+++ src/templates/request.js
@@ -1,83 +1,123 @@
-const nodemailer = require('nodemailer')
-const smtpTransport = require('nodemailer-smtp-transport')
-const changeEmailBody = require('../models/changeEmail')
-const resendEmail = require('../models/resendEmail')
-const inviteCollaborator = require('../models/inviteCollaborator')
-const disputedBody = require('../models/disputed')
-const main = require('../models/main')
+const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
+const changeEmailBody = require("../models/changeEmail");
+const resendEmail = require("../models/resendEmail");
+const inviteCollaborator = require("../models/inviteCollaborator");
+const disputedBody = require("../models/disputed");
+const main = require("../models/main");
+const statusUpdateBody = require("../models/statusUpdate");
 
-const transporter = nodemailer.createTransport(smtpTransport({
-	host: process.env.HOST_EMAIL,
-	port: 587,
-	secure: false,
-	auth: {
-		user: process.env.USER_EMAIL,
-		pass: process.env.USER_PASS
-	},
-	pool: true
-}));
+const userEmail = process.env.USER_EMAIL;
+const userPass = process.env.USER_PASS;
+
+const transporter = nodemailer.createTransport(
+  smtpTransport({
+    host: process.env.HOST_EMAIL,
+    port: 587,
+    secure: false,
+    auth: {
+      user: userEmail,
+      pass: userPass,
+    },
+    pool: true,
+  })
+);
 
 const htmlParser = (html) => {
-	return html.replace(/\<script(.*?)\>(.*?)\<\/script\>/g, '');
-}
+  return html.replace(/\<script(.*?)\>(.*?)\<\/script\>/g, "");
+};
 
-const request = async ({ to, subject, text, html, confirmEmail, inviteColaborator, disputed, changeEmail }) => {
-	let mailOptions = {
-		to,
-		text,
-		from: process.env.USER_EMAIL,
-		sender: process.env.USER_EMAIL,
-		replyTo: process.env.USER_EMAIL
-	};
-	try {
-		if (confirmEmail) {
-			if (confirmEmail.link) {
-				mailOptions['subject'] = 'Verifique seu e-mail';
-				mailOptions['html'] = resendEmail(confirmEmail.name, confirmEmail.link);
-			} else throw { msg: 'Link de confirmação é obrigatório', status: 400 };
-		}
-		else if (inviteColaborator) {
-			if (inviteColaborator.link) {
-				mailOptions['subject'] = 'Convite para se tornar um colaborador';
-				mailOptions['html'] = inviteCollaborator(inviteColaborator.name, inviteColaborator.supplier, inviteColaborator.link);
-			} else throw { msg: 'Link de cadastro é obrigatório', status: 400 };
-		}
-		else if (disputed) {
-			if (disputed.transaction) {
-				const { body, title, caption } = disputedBody(disputed.transaction);
-				mailOptions['subject'] = 'Notificação de disputa ⚠️';
-				mailOptions['html'] = main(body, title, caption);
-			} else throw { msg: 'Transação é obrigatória', status: 400 };
-		}
-		else if (changeEmail) {
-			if (changeEmail.newEmail) {
-				const { name, newEmail } = changeEmail;
-				mailOptions['subject'] = 'Seu e-mail para login foi alterado';
-				mailOptions['html'] = changeEmailBody(name, newEmail);
-			} else throw { msg: 'Novo email é obrigatório', status: 400 };
-		}
-		else {
-			let parsedHtml = html;
-			mailOptions['subject'] = subject;
-			mailOptions['text'] = text;
-			while (parsedHtml.includes('script')) {
-				parsedHtml = htmlParser(parsedHtml);
-			}
-			mailOptions['html'] = parsedHtml;
-		}
-		const result = await transporter.sendMail(mailOptions);
-		const response = { accepted: result.accepted, rejected: result.rejected, messageId: result.messageId };
-		return {
-			statusCode: 200,
-			body: JSON.stringify(response, null, 4)
-		}
-	} catch (error) {
-		console.log('Erro: ', error);
-		return {
-			statusCode: error.status || 500,
-			body: JSON.stringify({ error: { msg: error.msg || 'Erro na API de e-mail, tente novamente.' } }, null, 4)
-		}
-	}
-}
+const request = async ({
+  to,
+  subject,
+  text,
+  html,
+  confirmEmail,
+  inviteColaborator,
+  disputed,
+  changeEmail,
+  statusUpdate,
+}) => {
+  let mailOptions = {
+    to,
+    text,
+    from: userEmail,
+    sender: userEmail,
+    replyTo: userEmail,
+  };
+  try {
+    if (confirmEmail) {
+      if (confirmEmail.link) {
+        mailOptions["subject"] = "Verifique seu e-mail";
+        mailOptions["html"] = resendEmail(confirmEmail.name, confirmEmail.link);
+      } else throw { msg: "Link de confirmação é obrigatório", status: 400 };
+    } else if (inviteColaborator) {
+      if (inviteColaborator.link) {
+        mailOptions["subject"] = "Convite para se tornar um colaborador";
+        mailOptions["html"] = inviteCollaborator(
+          inviteColaborator.name,
+          inviteColaborator.supplier,
+          inviteColaborator.link
+        );
+      } else throw { msg: "Link de cadastro é obrigatório", status: 400 };
+    } else if (disputed) {
+      if (disputed.transaction) {
+        const { body, title, caption } = disputedBody(disputed.transaction);
+        mailOptions["subject"] = "Notificação de disputa ⚠️";
+        mailOptions["html"] = main(body, title, caption);
+      } else throw { msg: "Transação é obrigatória", status: 400 };
+    } else if (changeEmail) {
+      if (changeEmail.newEmail) {
+        const { name, newEmail } = changeEmail;
+        mailOptions["subject"] = "Seu e-mail para login foi alterado";
+        mailOptions["html"] = changeEmailBody(name, newEmail);
+      } else throw { msg: "Novo email é obrigatório", status: 400 };
+    } else if (statusUpdate) {
+      if (!statusUpdate?.status)
+        throw { msg: "Status da transação é obrigatório", status: 400 };
+      const { retailer, supplier, value, status } = statusUpdate;
+      mailOptions["subject"] = "Atualização de status";
+      const { body, caption, title } = statusUpdateBody(
+        retailer,
+        supplier,
+        value,
+        status
+      );
+      mailOptions["html"] = main(body, title, caption);
+    } else {
+      let parsedHtml = html;
+      mailOptions["subject"] = subject;
+      mailOptions["text"] = text;
+      while (parsedHtml.includes("script")) {
+        parsedHtml = htmlParser(parsedHtml);
+      }
+      mailOptions["html"] = parsedHtml;
+    }
+    const result = await transporter.sendMail(mailOptions);
+    const response = {
+      accepted: result.accepted,
+      rejected: result.rejected,
+      messageId: result.messageId,
+    };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response, null, 4),
+    };
+  } catch (error) {
+    console.log("Erro: ", error);
+    return {
+      statusCode: error.status || 500,
+      body: JSON.stringify(
+        {
+          error: {
+            msg: error.msg || "Erro na API de e-mail, tente novamente.",
+          },
+        },
+        null,
+        4
+      ),
+    };
+  }
+};
 
-module.exports = request
+module.exports = request;
